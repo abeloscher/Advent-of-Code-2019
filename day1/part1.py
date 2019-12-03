569d349e1d75081fdf2ab9303e6b68a76ec2d283
Abraham Loscher <abeloscher@gmail.com>
2019-12-02 23:35:57 -0500

    Part 1 complete

---
+++ day1/part1.py
@@ -0,0 +1,12 @@
+""" Solutions for Day 1  """
+import sys
+
+infile = open(sys.argv[1], "r")
+
+r_sum = 0
+
+for mass in infile:
+    massAsInt = int(mass)
+    r_sum += (massAsInt//3)-2
+
+print(r_sum)
