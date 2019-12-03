ac4bbf9e80e5bdb7de9be3ce0e8574d3937e96c6
Abraham Loscher <abeloscher@gmail.com>
2019-12-03 00:20:28 -0500

    Part 2, did a recursive solution to refresh a little on recursion

---
+++ day1/part2.py
@@ -0,0 +1,32 @@
+""" Solutions for Day 1  """
+import sys
+
+
+def calc_til_0(mass: int):
+    r_sum = 0
+    while (mass//3)-2 > 0:
+        mass = mass//3-2
+        r_sum += mass
+    return r_sum
+
+
+def calc_til_0_recurse_step(mass: int):
+
+    if mass <= 0:
+        return 0
+    return mass + calc_til_0_recurse_step((mass//3)-2)
+
+
+def calc_til_0_call_recurse(mass: int):
+    return calc_til_0_recurse_step(mass) - mass
+
+
+infile = open(sys.argv[1], "r")
+
+r_sum = 0
+
+for mass in infile:
+    massAsInt = int(mass)
+    r_sum += calc_til_0_call_recurse(massAsInt)
+
+print(r_sum)
